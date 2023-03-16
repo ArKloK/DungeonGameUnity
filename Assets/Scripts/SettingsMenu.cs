8197a1806905f3ef5b7dcdc5f54cb55639552270
ArKloK <ccamachobanez@gmail.com>
2023-03-16 21:05:49 +0100

    Settings Menu finished

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -9,12 +9,18 @@ public class SettingsMenu : MonoBehaviour
     public TMP_Dropdown resolutionDropdown;
     Resolution[] resolutions;
 
-    public Slider slider;
-    private float sliderValue;
-    public Image imagenMute;
+    private float sliderValueVolume;
+    public Slider sliderVolume;
+    public Image imageMute;
+
+    public Slider sliderBrigthness;
+    private float sliderValueBrightness;
+    public Image brightnessPanel;
+
+    public Toggle toggle;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         //**************Resolution Dropdown startup configuration***************
         resolutions = Screen.resolutions;
@@ -30,27 +36,81 @@ public class SettingsMenu : MonoBehaviour
         }
 
         resolutionDropdown.AddOptions(options);
+        CheckResolution();
 
         //***************Volume Slider startup configuration*******************
-        slider.value = PlayerPrefs.GetFloat("audioVolume", 0.5f);
-        AudioListener.volume = slider.value;
+        sliderVolume.value = PlayerPrefs.GetFloat("audioVolume", 0.5f);
+        AudioListener.volume = sliderVolume.value;
+        CheckMute();
+
+        //***************Brightness Slider startup configuration*******************
+        sliderBrigthness.value = PlayerPrefs.GetFloat("brightness", 0.35f);
+
+        brightnessPanel.color = new Color(brightnessPanel.color.r, brightnessPanel.color.g, brightnessPanel.color.b, sliderBrigthness.value);
+
+        //***************Full Screen Toggle startup configuration*******************
+        if (Screen.fullScreen)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+    }
+
+    public void ChangeSliderVolume()
+    {
+        sliderValueVolume = sliderVolume.value;
+        PlayerPrefs.SetFloat("audioVolume", sliderValueVolume);
+        AudioListener.volume = sliderVolume.value;
         CheckMute();
     }
 
-    public void ChangeSlider()
+    public void ChangeSliderBrightness()
     {
-        sliderValue = slider.value;
-        PlayerPrefs.SetFloat("audioVolume", sliderValue);
-        AudioListener.volume = slider.value;
-        CheckMute();
+        sliderValueBrightness = sliderBrigthness.value;
+        PlayerPrefs.SetFloat("brightness", sliderValueBrightness);
+        brightnessPanel.color = new Color(brightnessPanel.color.r, brightnessPanel.color.g, brightnessPanel.color.b, sliderBrigthness.value);
     }
 
     public void CheckMute()
     {
-        if (sliderValue == 0)
-            imagenMute.enabled = true;
+        if (sliderValueVolume == 0)
+            imageMute.enabled = true;
         else
-            imagenMute.enabled = false;
+            imageMute.enabled = false;
     }
 
+    public void ActivateFullScreen()
+    {
+        Screen.fullScreen = toggle.isOn;
+    }
+
+    public void CheckResolution()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int actualResolution = 0;
+
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (Screen.fullScreen && resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                actualResolution = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = actualResolution;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void ChangeResolution()
+    {
+        Resolution resolution = resolutions[resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 }
